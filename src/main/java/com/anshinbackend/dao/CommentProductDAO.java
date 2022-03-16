8ecb51a083e22e3dc8951c449e940258e865da2a
Admin <duydvph9704@fpt.edu.vn>
2022-03-17 02:10:02 +0700

    duy

--- src/main/java/com/anshinbackend/dao/CommentProductDAO.java
+++ src/main/java/com/anshinbackend/dao/CommentProductDAO.java
@@ -14,8 +14,8 @@ public interface CommentProductDAO extends JpaRepository<Comment, Integer> {
 
 
     List<Comment> findCommentsByProductId(int id);
-    @Query(value="Select * from Comments c where c.id_acount=?1")
+    @Query(value="Select c from Comments c where 1=1 and c.id_acount=?1",nativeQuery = true)
     List<Comment> findAllByAccount(Acount account);
-    @Query(value="Select * from Comments c where c.id_acount=?1 and c.id_product=?2")
+    @Query(value="Select c from Comments c where 1=1 and c.id_acount=?1 and c.id_product=?2",nativeQuery = true)
     List<Comment> findAllCommentsByAccountAndProduct(int accountId, int prouductId);
 }
